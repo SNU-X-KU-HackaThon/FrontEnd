c88abf9fde0346f347cfe9ef670b7d2cc2a2e3ed
Phia(가영) <76558220+rkdud007@users.noreply.github.com>
2022-01-15 19:13:32 +0900

    change

--- habit-haveit/pages/Signup.js
+++ habit-haveit/pages/Signup.js
@@ -1,30 +1,34 @@
 import React, { useState } from "react";
-import ErrorModal from './ErrorModal'
-import classes from './Signup.module.css'
-import Button from './Button'
+import ErrorModal from "./ErrorModal";
+import classes from "./Signup.module.css";
+import Button from "./Button";
 
-const signup = (props) => {
+const signup = () => {
   const [enteredUserid, setEnteredUserid] = useState("");
   const [enteredpassword, setEnteredpassword] = useState("");
   const [enteredpasswordcheck, setEnteredpasswordcheck] = useState("");
-  const [error,setError] = useState();
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
 
-    if (enteredUserid.trim().length === 0 || enteredpassword.trim().length === 0){
-        setError({
-            title: 'Invalid input',
-            message: '아이디와 비밀번호를 입력해주세요.'
-        })
+    if (
+      enteredUserid.trim().length === 0 ||
+      enteredpassword.trim().length === 0
+    ) {
+      setError({
+        title: "Invalid input",
+        message: "아이디와 비밀번호를 입력해주세요.",
+      });
     }
     if (enteredpassword != enteredpasswordcheck) {
-        setError({
-            title: 'Invalid password',
-            message: '비밀번호를 맞게 입력했는지 확인해주세요.'
-        })
+      setError({
+        title: "Invalid password",
+        message: "비밀번호를 맞게 입력했는지 확인해주세요.",
+      });
     }
-    props.onAddUser(enteredUserid, enteredpassword, enteredpasswordcheck);
+    // props.onAddUser(enteredUserid, enteredpassword, enteredpasswordcheck);
+    console.log(enteredUserid, enteredpassword, enteredpasswordcheck);
     setEnteredUserid("");
     setEnteredpassword("");
     setEnteredpasswordcheck("");
@@ -40,12 +44,17 @@ const signup = (props) => {
   };
   const errorHandler = () => {
     setError(null);
-
-}
+  };
 
   return (
     <div className={classes.input}>
-        {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <form onSubmit={addUserHandler}>
         <label htmlFor="id">아이디</label>
         <input
---
+++ habit-haveit/src/utils/api/index.jsx
@@ -0,0 +1,1 @@
+export { updateResourceApi, sendTxhash } from './imageApi'
