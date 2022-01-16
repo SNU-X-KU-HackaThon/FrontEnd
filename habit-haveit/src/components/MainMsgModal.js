417c80f26a9aafab47ab778380f56f5552831620
박채원 <nunu@bagchaewon-ui-MacBookAir.local>
2022-01-16 14:25:04 +0900

    commit

--- habit-haveit/src/components/MainMsgModal.js
+++ habit-haveit/src/components/MainMsgModal.js
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDom from "react-dom";
 import classes from "./MainMsgModal.module.css";
 import Button from "../components/Button"
+
 export default function MainMsgModal({ setModalSwitch }) {
   return ReactDom.createPortal(
     <div>
       <div className={classes.backdrop} onClick={() => setModalSwitch(false)} />
       <div className={classes.modal}>
         <div className={classes.content}>
-          <p>메세지함에 메세지가 없습니다!</p>
+          <img src='../letter.png'></img>
         </div>
         <div className={classes.actions}>
           <Button onClick={() => setModalSwitch(false)}>닫기</Button>
