8cfbac415795306ad4eb2e3a3dcbc35b84d08433
박채원 <nunu@bagchaewon-ui-MacBookAir.local>
2022-01-16 12:27:38 +0900

    eighth commit

--- habit-haveit/src/components/MainMsgModal.js
+++ habit-haveit/src/components/MainMsgModal.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDom from "react-dom";
 import classes from "./MainMsgModal.module.css";
-
+import Button from "../components/Button"
 export default function MainMsgModal({ setModalSwitch }) {
   return ReactDom.createPortal(
-    <div className={classes.ModalBackground}>
-      <div className={classes.ModalBox}>
-        <div onClick={() => setModalSwitch(false)}>x</div>
-        <p>msg today</p>
+    <div>
+      <div className={classes.backdrop} onClick={() => setModalSwitch(false)} />
+      <div className={classes.modal}>
+        <div className={classes.content}>
+          <p>No messages</p>
+        </div>
+        <div className={classes.actions}>
+          <Button onClick={() => setModalSwitch(false)}>닫기</Button>
+        </div>
       </div>
     </div>,
     document.getElementById("modal-root")
